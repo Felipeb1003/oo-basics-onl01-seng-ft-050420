c6ff4e1575bc315d6eea2a6b0db1fce9b594e804
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-13 17:46:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,12 +5,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
-  def condition=(condition)
-    @condition= condition
-  end
-  def condition
-    @condition
-  end
+
   def cobble
    @condition = "new"
     puts "Your shoe is as good as new!"
